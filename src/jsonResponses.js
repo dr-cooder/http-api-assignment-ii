4f93184042a04b7e6fb0bc9ecf59be2536d721cd
Felix Warner <fwarner2319@gmail.com>
2022-09-22 02:18:38 -0400

    Created server with responses, started POST behavior of nameForm

---
+++ src/jsonResponses.js
@@ -0,0 +1,20 @@
+const respond = (request, response, status, object) => {
+  response.writeHead(status, {
+    'Content-Type': 'application/json',
+  });
+  response.write(JSON.stringify(object));
+  response.end();
+};
+
+const notFound = (request, response) => {
+  const responseJSON = {
+    message: 'The page you are looking for was not found.',
+    id: 'notFound',
+  };
+
+  return respond(request, response, 404, responseJSON);
+};
+
+module.exports = {
+  notFound,
+};
---
+++ src/server.js
@@ -0,0 +1,30 @@
+const http = require('http');
+const url = require('url');
+const htmlHandler = require('./htmlResponses.js');
+const jsonHandler = require('./jsonResponses.js');
+
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+const urlStruct = {
+  '/': htmlHandler.getIndex,
+  '/style.css': htmlHandler.getCSS,
+  notFound: jsonHandler.notFound,
+};
+
+const onRequest = (request, response) => {
+  const parsedUrl = url.parse(request.url, true);
+  const handlerFunction = urlStruct[parsedUrl.pathname];
+  const { query } = parsedUrl;
+  const acceptedTypes = request.headers.accept.split(',');
+  console.log(request.method, parsedUrl.path, acceptedTypes);
+
+  if (handlerFunction) {
+    handlerFunction(request, response, query);
+  } else {
+    urlStruct.notFound(request, response, query);
+  }
+};
+
+http.createServer(onRequest).listen(port, () => {
+  console.log(`Listening on 127.0.0.1:${port}`);
+});
